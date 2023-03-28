99957950237640347a66e44bbcdd4ebf214671aa
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-03-28 10:50:38 -0700

    Add HTML elements using Javascript

--- client/public/script.js
+++ client/public/script.js
@@ -9,7 +9,18 @@ const loadEvent = function() {
 
   // Write your JavaScript code after this line
 
-  
+  const header3 = document.createElement("h3");
+  const header3Text = document.createTextNode("Level 3 subtitle");
+  header3.appendChild(header3Text);
+
+  const mainHeader = document.getElementById("header");
+  mainHeader.insertAdjacentElement("beforeend", header3);
+
+  const bodyHTML = document.getElementById("body");
+  for (const regard of greetings) {
+    bodyHTML.insertAdjacentHTML("beforeend", divElement(regard));
+  }
+
   // Write your JavaScript code before this line
 
 }
