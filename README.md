7ffcc40f04683a56f185849cd64f6362c3efdfb9
appuser <appuser@journey2-95cdcd64d-8dd4r>
2023-03-28 05:48:04 +0000

    starter project

---
+++ README.md
@@ -0,0 +1,1 @@
+# view-around-us
---
+++ client/public/script.js
@@ -0,0 +1,17 @@
+
+const greetings = ["Hello", "Bye", "Ciao", "Cheers", "Bests"];
+
+const divElement = function (content){
+  return `<div> ${content} </div>`;
+}
+
+const loadEvent = function() {
+
+  // Write your JavaScript code after this line
+
+  
+  // Write your JavaScript code before this line
+
+}
+
+window.addEventListener("load", loadEvent);
